efdeff761159f81f152c3379bd621f0ee8e5530e
otakao <kazuhawk@gmail.com>
2020-07-25 21:41:38 +0900

    data-complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
 
-  resources :posts , only: [:index, :create]
+  resources :posts , only: [:index, :new, :create]
   resources :maps , only: [:index, :new, :create]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
