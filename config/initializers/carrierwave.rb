0e5e8a40a1c059ffb58eb06eee8709497ea58b52
otakao <kazuhawk@gmail.com>
2020-08-17 20:50:55 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,16 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
    config.storage = :file
-  elsif Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory  = 'takao'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takao'
+  # elsif Rails.env.production?
+  #   config.storage = :fog
+  #   config.fog_provider = 'fog/aws'
+  #   config.fog_credentials = {
+  #     provider: 'AWS',
+  #     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  #     aws_secret_access_key: Rails.Rails.application.credentials.aws[:secret_access_key],
+  #     region: 'ap-northeast-1'
+  #   }
+  #   config.fog_directory  = 'takao'
+  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takao'
   end
 end
